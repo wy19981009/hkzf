670d9514b67a1d7a47f513989f49d6764ec9d50f
yuxigaohuang <1070999146@qq.com>
2022-12-31 16:46:23 +0800

    顶部导航栏添加props校验

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -5,6 +5,9 @@ import { NavBar } from "antd-mobile";
 // 导入withRouter高阶组件
 import { withRouter } from "react-router-dom";
 
+// 导入props校验
+import PropTypes from "prop-types";
+
 import './index.scss'
 
 
@@ -21,6 +24,11 @@ function NavHeader({ children, history, onLeftClick }) {
             {children}
         </NavBar>
     )
+}
+
+NavHeader.propTypes = {
+    children: PropTypes.string.isRequired,
+    onLeftClick: PropTypes.func
 }
 
 // withRouter的返回值也是一个组件
