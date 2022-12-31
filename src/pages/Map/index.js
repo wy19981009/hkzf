ef1f6cfca2a7a082c3aabe8779b8c02ff9b58ab2
yuxigaohuang <1070999146@qq.com>
2022-12-31 17:38:39 +0800

    根据定位展示当前城市

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -6,15 +6,42 @@ import NavHeader from "../../components/NavHeader";
 // import './index.scss'
 import styles from './index.module.css'
 
+const BMapGL = window.BMapGL
+
 export default class Map extends React.Component {
 
     componentDidMount() {
+        this.initMap();
+    }
+
+    // 初始化地图
+    initMap() {
+        // 获取当前定位城市
+        const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'));
+        console.log(label, value);
+
         // 初始化地图实例
         // 在react脚手架中全局对象需要使用window来访问，否则会造成ESlint报错
-        const map = new window.BMapGL.Map('container');
+        const map = new BMapGL.Map('container');
         // 设置中心点坐标
-        const point = new window.BMapGL.Point(116.404, 39.915);
-        map.centerAndZoom(point, 15);
+        // const point = new window.BMapGL.Point(116.404, 39.915);
+
+        //创建地址解析器实例
+        const myGeo = new BMapGL.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野
+        myGeo.getPoint(label, (point) => {
+            if (point) {
+                map.centerAndZoom(point, 11);
+                map.addControl(new BMapGL.ZoomControl())
+                map.addControl(new BMapGL.ScaleControl())
+
+            } else {
+                alert('您选择的地址没有解析到结果！');
+            }
+        }, label)
+
+        // 初始化地图
+        // map.centerAndZoom(point, 11);
     }
 
     render() {
