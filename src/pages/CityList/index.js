2507b62b7ac0f31d9fead61cd6ec533d31a2dc35
yuxigaohuang <1070999146@qq.com>
2022-12-31 10:55:12 +0800

    城市列表页面顶部导航栏

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+
+import './index.scss'
+
+import { NavBar } from 'antd-mobile';
 export default class CityList extends React.Component {
     render() {
-        return <div>城市列表</div>
+        return (
+            <div className="citylist">
+                {/* 顶部导航栏 */}
+                <NavBar
+                    className="navBar"
+                    mode="light"
+                    icon={<i className="iconfont icon-back"/>}
+                    onLeftClick={() => this.props.history.go(-1)}>城市选择</NavBar>
+            </div>
+        )
     }
 }
