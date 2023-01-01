67dcfd6feec8309469ab5fd4ce54115a23be5d2b
yuxigaohuang <1070999146@qq.com>
2023-01-01 13:32:05 +0800

    渲染所有区的房源信息

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -3,6 +3,9 @@ import React from "react";
 // 导入顶部导航栏组件
 import NavHeader from "../../components/NavHeader";
 
+// 导入axios
+import axios from 'axios';
+
 // import './index.scss'
 import styles from './index.module.css'
 
@@ -29,7 +32,6 @@ export default class Map extends React.Component {
     initMap() {
         // 获取当前定位城市
         const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'));
-        console.log(label, value);
 
         // 初始化地图实例
         // 在react脚手架中全局对象需要使用window来访问，否则会造成ESlint报错
@@ -40,19 +42,61 @@ export default class Map extends React.Component {
         //创建地址解析器实例
         const myGeo = new BMapGL.Geocoder();
         // 将地址解析结果显示在地图上，并调整地图视野
-        myGeo.getPoint(label, (point) => {
+        myGeo.getPoint(label, async (point) => {
             if (point) {
                 map.centerAndZoom(point, 11);
                 map.addControl(new BMapGL.ZoomControl())
                 map.addControl(new BMapGL.ScaleControl())
 
-                const opts = {
-                    position: point,
-                    offset: new BMapGL.Size(-35, -35)
-                }
+                // 获取房源数据
+                const res = await axios.get(`http://localhost:8080/area/map?id=${value}`)
+                // console.log(res);
+                res.data.body.forEach(item => {
+
+                    const { coord: { longitude, latitude }, label: areaName, count, value } = item
+
+                    const areaPoint = new BMapGL.Point(longitude, latitude)
+                    // 创建label实例对象
+                    const label = new BMapGL.Label('', {
+                        position: areaPoint,
+                        offset: new BMapGL.Size(-35, -35)
+                    })
+
+                    // 添加唯一标识
+                    label.id = value
+
+                    // 设置房源覆盖物内容
+                    label.setContent(`
+                    <div class="${styles.bubble}">
+                        <p class="${styles.name}">${areaName}</p>
+                        <p>${count}套</p>
+                    </div>
+                `)
+                    // 调用setstyle()方法设置样式
+                    label.setStyle(labelStyle)
+
+                    // 点击事件
+                    label.addEventListener('click', () => {
+                        console.log('房源覆盖物被点击了', label.id);
+                        // 放大地图
+                        map.centerAndZoom(areaPoint, 13)
+
+                        // 清除当前覆盖物信息
+                        map.clearOverlays()
+                    })
+
+                    // 在map对象上调用addOverlay()方法，将文本覆盖物调价到地图中
+                    map.addOverlay(label)
+
+                })
+
+
 
                 // 创建label实例对象
-                const label = new BMapGL.Label('', opts)
+                const label = new BMapGL.Label('', {
+                    position: point,
+                    offset: new BMapGL.Size(-35, -35)
+                })
 
                 // 设置房源覆盖物内容
                 label.setContent(`
