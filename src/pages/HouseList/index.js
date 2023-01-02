be3c853a8d793696c7b45bbb7836bf5c5c718445
yuxigaohuang <1070999146@qq.com>
2023-01-02 16:48:32 +0800

    首页导入搜索栏组件，优化代码

--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -15,7 +15,10 @@ export default class HouseList extends React.Component {
 		return (
 			<div>
 				<Flex className={styles.header}>
-					<i className='iconfont icon-back'></i>
+					<i
+						className='iconfont icon-back'
+						onClick={() => this.props.history.go(-1)}
+					></i>
 					<SearchHeader cityName={label} className={styles.searchHeader} />
 				</Flex>
 			</div>
