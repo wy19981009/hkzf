66dbbfca5fc1124f87f4a0a8e30163a30a8a65f3
yuxigaohuang <1070999146@qq.com>
2023-01-04 09:55:14 +0800

    优化antd-mobile样式

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // 导入antd-mobile的样式
-import "antd-mobile/dist/antd-mobile.css";
+// import "antd-mobile/dist/antd-mobile.css";
 // 导入字体图标样式
 import "./assets/fonts/iconfont.css";
-// 导入自己的样式
-import "./index.css";
 
 // 导入react-virtualized组件的样式
 import "react-virtualized/styles.css";
@@ -19,5 +17,7 @@ root.render(
 ); */
 
 import App from "./App";
+// 导入自己的样式
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
